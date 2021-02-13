2ac40e40727da7e5e061c643cf578581e38fe5cc
Aaron Lin <aaronlin256@gmail.com>
2021-02-13 01:17:53 -0500

    work one adding ticker data to message reply

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    const tickerMatch = msg.content.match(/\$[a-zA-Z.]+/)
+    const tickerMatch = msg.content.match(/(\$[a-zA-Z.]+)/)
     console.log(tickerMatch)
     if (tickerMatch.length > 0) {
-        message.channel.send(tickerMatch[0]);
+        let editedText = msg.toString().replace(/\$[a-zA-Z.]+/, "**\$1**")
+        msg.edit(editedText)
+        // you can do whatever you want with the msg variable; it returns the normal message object.
+        msg.react("👋")
+        // message.channel.send(tickerMatch[0]);
     }
 });
 
